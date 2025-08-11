956706dfa8e7bb79fe0006e89e471825c0c89b78
alchonreyes <alchonreyez@gmail.com>
2025-08-11 21:22:38 +0800

    unfinish 10% progress navbar

---
+++ includes/navbar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../includes/navbar.css">
+</head>
+<body>
+    <nav>
+        <div class="nav-section">
+            <div class="logo">
+                LOGO
+            </div>
+            <div class="nav-links">
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Brands</a></li>
+                </ul>
+                <div class="search">
+                    <input type="text" id="browse" placeholder="Im looking for....">
+                </div>
+            </div>
+            <div class="nav-config">
+                
+            </div>
+                   
+         </div>
+    </nav>
+</body>
+</html>
