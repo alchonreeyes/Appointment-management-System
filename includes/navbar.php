81f5d877f943d9d12052c8346255604a8f6a9ca1
alchonreyes <alchonreyez@gmail.com>
2025-08-12 21:37:19 +0800

    ...

--- includes/navbar.php
+++ includes/navbar.php
@@ -37,6 +37,16 @@
                 <a href="#" class="book-btn">Book Appointment</a>
             </div>
         </div>
+        <div class="dropdown-content">
+            <ul class="brands">
+                asd
+                <br>
+                adfbasdf
+            </ul>
+            <ul class="brands">
+                asd
+            </ul>
+        </div>
     </nav>
 </body>
 </html>
