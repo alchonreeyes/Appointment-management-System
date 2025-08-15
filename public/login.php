65ae8400fc99ee31157c9f47707af9fc17f4ae6a
alchonreyes <alchonreyez@gmail.com>
2025-08-15 16:45:48 +0800

    abt

--- public/login.php
+++ public/login.php
@@ -10,8 +10,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="../assets//login.css">
 </head>
-<body>
-    
+<body   >
+    <?php include '../includes/navbar.php' ?>
+    <div>
+        asdasd
+    </div>
+    <?php include '../includes/footer.php' ?>
 </body>
 </html>
