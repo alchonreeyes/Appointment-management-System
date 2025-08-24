9a1cc93b1c2f168c46b87bec553a8e2116429616
alchon <alchonreyez@gmail.com>
2025-08-24 12:03:22 +0800

    very small changes navbar ===>>

--- includes/navbar.php
+++ includes/navbar.php
@@ -16,10 +16,6 @@
     <!-- Main nav -->
     <nav>
         <div class="nav-section">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> feature-navbar
             <!-- Logo container with dropdown - CHANGED -->
             <div class="logo-container">
                 <div class="logo">
@@ -79,13 +75,6 @@
                         </ul>    
                     </div>
                 </div>
-<<<<<<< HEAD
-=======
-            <div class="logo">
-                <img src="" alt="Logo">
->>>>>>> 65cb1bbdee153c107f4426ad9b4be1548d227da1
-=======
->>>>>>> feature-navbar
             </div>
 
             <div class="nav-links">
@@ -156,14 +145,9 @@
                 </ul>    
             </div>
         </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
         </div>
-       
->>>>>>> 65cb1bbdee153c107f4426ad9b4be1548d227da1
-=======
->>>>>>> feature-navbar
+
     </nav>
 </body>
 </html>
