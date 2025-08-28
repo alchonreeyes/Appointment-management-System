382436874dd5efc858fe0a92b016a9d5e3740180
alchon <alchonreyez@gmail.com>
2025-08-28 11:00:03 +0800

    typo

--- actions/login-action.php
+++ actions/login-action.php
@@ -10,7 +10,8 @@ if (isset($_POST['login'])) {
     $password = $_POST['password'];
 
     $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
-    $stmt->execute(['$email']);
+    $stmt->execute([$email]);
+    
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
     if ($user && password_verify($password, $user['password_hash'])) {
         $_SESSION['user_id'] = $user['id'];
