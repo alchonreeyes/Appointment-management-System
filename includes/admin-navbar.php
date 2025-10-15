38da2756a1f60419311ed91d24b01acf87cc903c
alchonreyees <alchonreyez@gmail.com>
2025-10-15 18:19:36 +0800

    finishing navbar for admin side

---
+++ includes/admin-navbar.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../includes/admin-navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+    <!-- Top bar -->
+    <div class="top-bar">
+        <span>Store Location | Brand</span>
+        <a href="#">Contact Us</a>
+    </div>
+
+    <!-- Main nav -->
+    <nav>
+        <div class="nav-section">
+            <!-- Logo container with dropdown - CHANGED -->
+            <div class="logo-container">
+            <div class="logo">
+                <img src="" alt="Logo">
+            </div>
+            
+            <!-- Dropdown moved inside logo-container -->
+            <div class="dropdown">
+                <div class="dropdown-content">
+                <ul>
+                    <h4>BRANDS</h4>
+                    <li><a href="#">Coach</a></li>
+                    <li><a href="#">Armani Exchange</a></li>
+                    <li><a href="#">ARNETTE</a></li>
+                    <li><a href="#">Celine</a></li> 
+                    <li><a href="#">Roman King</a></li> 
+                    <li><a href="#">C. Lindbergh</a></li>
+                    <br>
+                </ul>
+                <ul>
+                    <li><a href="">Memoflex</a></li>
+                    <li><a href="">Kate Spade New York</a></li>
+                    <li><a href="">Herman Miller</a></li>
+                    <li><a href="">Hushpuppies</a></li>
+                    <li><a href="">Jiashie eyes</a></li>
+                    <li><a href="#">Airflex</a></li>    
+                </ul>
+                 <ul>
+                    <h4>EYEWEAR FRAMES</h4>
+                    <li><a href="#">Acetate</a></li>
+                    <li><a href="#">B Titanium</a></li>
+                    <li><a href="#">Metal</a></li>
+                    <li><a href="#">Plastic</a></li>    
+                    <li><a href="#">Plastic/Metal</a></li>    
+                    <li><a href="#">Rubber</a></li>    
+                    <br>
+                </ul>
+                <ul>
+                    <li><a href="#">Rubber/Metal</a></li>
+                    <li><a href="#">Rubber/Plastic</a></li>
+                    <li><a href="#">Titanium</a></li>
+                    <li><a href="#">Titanium/Acatate</a></li>
+                    <li><a href="#">Titanium/Wood</a></li>
+                    <li><a href="#">Wood</a></li>
+                </ul>
+                <ul>
+                    <h4>LENSES</h4>
+                    <li><a href="#">Apollo Lenses</a></li>
+                    <li><a href="#">Essilor Lenses</a></li>
+                    <li><a href="#">Hoya Lenses</a></li>
+                    <br>
+                    <br>
+                    
+                    <br>
+                    <br>
+                    <br>
+                </ul>    
+                </div>
+            </div>
+            </div>
+
+            <div class="nav-links">
+            
+            </div>
+
+            <div class="nav-config">
+            <ul class="nav-configs">
+                <li><a href="#"><i class="fa-solid fa-circle-info"></i> Dashboard</a></li>
+                <li><a href="#"><i class="fa-solid fa-list"></i>Patient's record</a></li>
+                <li><a href="#"><i class="fa-solid fa-star"></i> Product</a></li>
+            </ul>
+            
+            <i class="fa-solid fa-user"></i>
+            <a href="#" class="signin">Account</a>
+            <a href="#" class="book-btn">Profile</a>
+            </div>
+        </div>
+
+
+    </nav>
+</body>
+</html>
