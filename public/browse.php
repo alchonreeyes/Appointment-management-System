44af3acf4fda8c79d71d6f2bfcc923629b788ec0
alchonreyees <alchonreyez@gmail.com>
2025-10-19 13:08:47 +0800

    HAHAHHA

---
+++ public/browse.php
@@ -0,0 +1,21 @@
+<?php 
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Browse</title>
+</head>
+<body>
+    <?php include '../includes/navbar.php' ?>
+
+    
+
+
+    <?php include '../includes/footer.php' ?>
+</body>
+</html>
