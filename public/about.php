9f369f8659b01401b102b97815b721e4b47b4932
alchonreyees <alchonreyez@gmail.com>
2025-10-30 15:24:52 +0800

    making about web page

--- public/about.php
+++ public/about.php
@@ -16,7 +16,7 @@
 
     <div class="route-link">
         <div class="route-wrapper">
-            <a href="">Home</a>
+            <a href="../public/home.php">Home</a>
             <h2>></h2>
             <a href="">About</a>
         </div>
@@ -25,9 +25,32 @@
     <div class="hero-section">
         
     </div>
+    <div class="eyemaster_wrapper">
+        <div class="container">
+            <h1><span>"</span>ABOUT EYE-MASTER<span>"</span></h1>      
+            
+            <p>Eye Master Optical Clinic provides eye care services such as eye examinations, medical certificates, prescription glasses, contact lenses, and optical products.</p>
+            <p>By booking an appointment or availing of our services, you agree to provide accurate and truthful personal information. Misuse or fraudulent use of our systems is strictly prohibited.</p>
+            <br>
+            <h4>How We Use Your Information - We use your data to:</h4>
+            <p>Schedule and manage your appointments
+Provide personalized eye care and prescriptions
+Issue medical certificates when applicable
+Send important updates or health-related promotions
+Improve our customer service and clinic operations
+.
+</p>  
+<p>- When you schedule an appointment, we may contact you through SMS, phone call, or email to confirm your booking, send reminders, or inform you of product availability and promotions.</p>    
+        </div>
+
+        <div class="general-row">
+            <h1>sads</h1>
+        </div>
+    </div>
+
+asdasd
 
 
-
-
+<?php include '../includes/footer.php'; ?>
 </body>
 </html>
