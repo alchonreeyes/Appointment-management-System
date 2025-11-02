3c31a7e5410b7eac5c06e42a879a3e923d77d421
alchonreyees <alchonreyez@gmail.com>
2025-11-02 13:58:00 +0800

    merging complete admin and staff

---
+++ public/store.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Store</title>
+</head>
+<body>
+    <?php include '../includes/navbar.php'; ?>
+    
+    
+    
+    <?php include '../includes/footer.php'; ?>
+</body>
+</html>
