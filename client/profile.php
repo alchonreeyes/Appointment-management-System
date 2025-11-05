c8bb8d337ace152e1e569d2adc2bb7be53ebd65c
alchonreyees <alchonreyez@gmail.com>
2025-11-05 21:36:12 +0800

    about to make client configuration

--- client/profile.php
+++ client/profile.php
@@ -7,18 +7,6 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
-// Optional: fetch user data from database to display in profile
-include '../config/db.php';
-$db = new Database();
-$pdo = $db->getConnection();
-
-$stmt = $pdo->prepare("SELECT * FROM users WHERE id = ?");
-$stmt->execute([$_SESSION['user_id']]);
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-if (!$user) {
-    die("User not found in database.");
-}
 ?>
 
 <!DOCTYPE html>
@@ -27,9 +15,21 @@ if (!$user) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
+    <link rel="stylesheet" href="./style/profile.css">
 </head>
 <body>
     <?php include '../includes/navbar.php' ?>
+    <div class="link-section">
+        <a href="../public/home.php"><i class="fa-solid fa-house"></i></a>
+        <i class="fa-solid fa-arrow-right"></i>
+        <p>Profile</p>
+    </div>
+    <div class="profile">
+        <div class="profile-details">
+            asdsds
+        </div>
+
+    </div>
     <?php include '../includes/footer.php' ?>
 </body>
 </html>
